33f4a15f6f82dbf419ce3c322b669f504aa91ae7
olomg48 <olomg48@gmail.com>
2026-01-10 10:38:14 +0100

    infra done

--- inference_service/app/main.py
+++ inference_service/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import mlflow.sklearn
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
+print("test")
+mlflow.set_tracking_uri("http://mlflow:5000")
+
+model_uri = "models:/TodoClassifier@champion"
+loaded_model = mlflow.sklearn.load_model(model_uri)
 
 @app.get("/health")
 def health():
@@ -8,4 +14,6 @@ def health():
 
 @app.post("/predict")
 def prediction(task: str):
-    return "testreturn"
+    prediction = loaded_model.predict([task])
+    return prediction[0]
+    #return("siematest")
---
+++ ml/train_and_push.py
@@ -0,0 +1,38 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+import os
+
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://localhost:9000"
+os.environ['AWS_ACCESS_KEY_ID'] = "admin"
+os.environ['AWS_SECRET_ACCESS_KEY'] = "password123"
+
+mlflow.set_tracking_uri("http://169.254.53.144:5000")
+mlflow.set_experiment("TodoClassifier")
+#mlflow.sklearn.autolog()
+print(f"Tracking URI: {mlflow.get_tracking_uri()}")
+print(f"Registry URI: {mlflow.get_registry_uri()}")
+
+df = pd.read_csv('data.csv')
+X_train, X_test, y_train, y_test = train_test_split(df['Task'], df['Category'], test_size=0.35, random_state=19)
+
+
+with mlflow.start_run():
+    model = Pipeline([
+        ('tfidf', TfidfVectorizer()),
+        ('clf', LinearSVC())
+    ])
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    mlflow.log_artifact("test.txt")
+    acc = accuracy_score(y_test, y_pred)
+    precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='weighted')
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
+    mlflow.sklearn.log_model(sk_model=model, artifact_path="model", registered_model_name="TodoClassifier" )
