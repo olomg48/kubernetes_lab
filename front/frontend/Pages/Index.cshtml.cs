f620805a5f2358dd177590adaa839656c0094d47
olomg48 <olomg48@gmail.com>
2026-05-01 15:51:28 +0200

    frontent structured logs

--- front/frontend/Pages/Index.cshtml.cs
+++ front/frontend/Pages/Index.cshtml.cs
@@ -1,47 +1,48 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using frontend.Contracts;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization; 
-namespace IIS_website.Pages
-{
-
-    public record TaskCreateDto
-{
-    [Required, MinLength(1)]
-    [JsonPropertyName("description")] 
-    public string description { get; set; } = "";
-    public string category { get; set; } = "default";
-}
-    public class IndexModel : PageModel
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Serilog;
+    using frontend.Contracts;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.Json.Serialization; 
+    namespace frontend.Pages
     {
-        private readonly IHttpClientFactory _httpFactory;
 
-        public List<TaskReadDto> ItemsList { get; private set; } = new();
-        
-        [BindProperty]
-        public TaskCreateDto ItemCreate { get; set; } = new();
-        
-
-        public IndexModel(IHttpClientFactory httpFactory)
-        {
-            _httpFactory = httpFactory;
-        }
-
-        public async Task OnGetAsync()
-        {
-            var client = _httpFactory.CreateClient("api");
-            ItemsList = await client.GetFromJsonAsync<List<TaskReadDto>>("/tasks/list")
-                        ?? new List<TaskReadDto>();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid) return Page();
-            var client = _httpFactory.CreateClient("api");
-            var response = await client.PostAsJsonAsync("/tasks/create", ItemCreate);
-            return RedirectToPage();
-        }
+        public record TaskCreateDto
+    {
+        [Required, MinLength(1)]
+        [JsonPropertyName("description")] 
+        public string description { get; set; } = "";
+        public string category { get; set; } = "default";
     }
+        public class IndexModel : PageModel
+        {
+            private readonly IHttpClientFactory _httpFactory;
 
-}
+            public List<TaskReadDto> ItemsList { get; private set; } = new();
+            
+            [BindProperty]
+            public TaskCreateDto ItemCreate { get; set; } = new();
+            
+
+            public IndexModel(IHttpClientFactory httpFactory)
+            {
+                _httpFactory = httpFactory;
+            }
+
+            public async Task OnGetAsync()
+            {
+                var client = _httpFactory.CreateClient("api");
+                ItemsList = await client.GetFromJsonAsync<List<TaskReadDto>>("/tasks/list")
+                            ?? new List<TaskReadDto>();
+            }
+
+            public async Task<IActionResult> OnPostAsync()
+            {
+                if (!ModelState.IsValid) return Page();
+                var client = _httpFactory.CreateClient("api");
+                var response = await client.PostAsJsonAsync("/tasks/create", ItemCreate);
+                return RedirectToPage();
+            }
+        }
+
+    }
--- front/frontend/Program.cs
+++ front/frontend/Program.cs
@@ -1,16 +1,31 @@
 using frontend;
 using Prometheus;
-
+using Serilog;
+using Serilog.Templates;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        // 1. Najpierw czytamy poziomy logowania (Information, Warning itp.) z appsettings.json
+        .ReadFrom.Configuration(context.Configuration)
+        
+        // 2. Potem w kodzie konfigurujemy konsolę i nasz wymarzony szablon
+        .WriteTo.Console(new ExpressionTemplate(
+            "{ { '@timestamp': @t, log_level: @l, message: @m, exception: @x, RequestId: RequestId } }\n"
+        ));
+});
 
 builder.Services.AddHttpClient("api", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]);
 });
-
+  
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpMetrics();
 
